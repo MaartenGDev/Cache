da77db2b8bdfe8843599c671efc3c4203d028f33
MaartenGDev <maarten.grootoonk@gmail.com>
2016-10-01 21:34:24 +0200

    Added check for false return

--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -15,4 +15,13 @@ class StorageTest extends PHPUnit_Framework_TestCase
 
         $cacheItem = $localStorage->save('test_cache_in_invalid_directory','Hello World');
     }
+
+    public function testStorageCacheItemInInvalidDirectoryReturnsFalse(){
+        $dir = dirname(__FILE__)  . '/../invalid_directory/';
+        $localStorage = new LocalDriver($dir);
+
+        $cacheItem = @$localStorage->save('test_cache_in_invalid_directory','Hello World');
+
+        $this->assertFalse($cacheItem);
+    }
 }
