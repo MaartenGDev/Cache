d69084c62cffe42dfdf9084eda21401d09140bdd
MaartenGDev <maarten.grootoonk@gmail.com>
2016-10-01 21:25:57 +0200

    Added Storage related tests

---
+++ tests/StorageTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace MaartenGDev\Tests;
+
+use MaartenGDev\LocalDriver;
+use PHPUnit_Framework_Error_Warning;
+use PHPUnit_Framework_TestCase;
+
+class StorageTest extends PHPUnit_Framework_TestCase
+{
+    public function testStorageCacheItemInInvalidDirectory(){
+      $this->expectException(PHPUnit_Framework_Error_Warning::class);
+
+        $dir = dirname(__FILE__)  . '/../invalid_directory/';
+        $localStorage = new LocalDriver($dir);
+
+        $cacheItem = $localStorage->save('test_cache_in_invalid_directory','Hello World');
+    }
+}
